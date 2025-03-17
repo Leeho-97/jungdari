cb0b4a92f355abeca0aa4409722c577f56aaf5ff
lee hyung gyu <sometime8557@gmail.com>
2025-03-17 17:55:00 +0900

    footer 정리

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import './reset.css';
 import Navbar from './components/Navbar';
 // import MainC from './components/MainC';
 import './Test.css'
+import Text from './components/Test'
 
 function App() {
 
 
   return (
-    <div className='full'>
-    <Navbar/>
     <div className='wrapper'>
-      <div className='layout'>
-        <div className='mainC'>컨텐츠</div>
+      <div className='container'>
+        <Navbar/>
+        <div className='layout'>
+            <div className='mainC'>
+              <Text/>
+            </div>
+        </div>
         <div className='footer'>풋터</div>
       </div>
-    </div>
-
-
     </div>
   )
 }
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -2,7 +2,7 @@
 
 function Test() {
   return (
-    <div>Test</div>
+    <div style={{width :"100% ", height : "1200px", backgroundColor : 'brown'}}>Test</div>
   )
 }
 
