f7ae6956b777c2c7bb18b2cd63f46b027e4a2df4
lee hyung gyu <sometime8557@gmail.com>
2025-03-26 16:52:06 +0900

    라우트 페이지 이동

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 import Jlogo from '../imgs/j_logo.svg';
 import styled from "styled-components";
 
 function Navbar() {
 
-  const logoAction =()=>{
-    console.log("로고 동작 확인");
+  const navigate = useNavigate();
+
+  const goPage =(location)=>{
+    
+    navigate(`/${location}`, {state : {orderId : 1}})
   }
+
 
   return (
     <header>
         <div style={{backgroundColor : "#f7f7f7" , width : "100%", height : "40px"}}></div>
         <div style={{backgroundColor : "white", width : "100%", height : "120px", boxShadow : "0px 10px 20px  rgba(0, 0, 0, 0.1)", display : "flex", justifyContent : "center"}}>
             <div style={{width : "1400px", height : "120px", display : "flex", justifyContent : "space-between" }}>
-                <img src={Jlogo} alt='중달이 로고' style={{width : "9rem", height : "auto", cursor :"pointer"}} onClick={logoAction}/>
+                <img src={Jlogo} alt='중달이 로고' style={{width : "9rem", height : "auto", cursor :"pointer"}} onClick={()=>{goPage('')}}/>
                   <HCDiv>
                   <HCButton>구매대행</HCButton>
                   <HCButton>배송대행</HCButton>
@@ -20,7 +26,7 @@ function Navbar() {
                   <HCButton>MD Pick</HCButton>
                   </HCDiv>
                 <HEDiv>
-                  <HEButton>마이페이지</HEButton>
+                  <HEButton onClick={()=>{goPage('mypage')}}>마이페이지</HEButton>
                   <HEButton>장바구니</HEButton>
                   <HEButton>로그아웃</HEButton>
                 </HEDiv>
